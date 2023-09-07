4e36400943eeac1222f58de6925deb052850c619
sachin_vedaraj <sachinkapalidigi@gmail.com>
2023-09-07 13:18:49 +0530

    chore: update constants

--- src/constants/imageResponse.js
+++ src/constants/imageResponse.js
@@ -1,10 +1,12 @@
 const IMAGE_RESPONSE = {
   SAVE_SUCCESS: {
     status: "success",
+    code: "SAVE_SUCCESS",
     message: "Image saved successfully.",
   },
   UPDATE_SUCCESS: {
     status: "success",
+    code: "UPDATE_SUCCESS",
     message: "Image updated successfully.",
   },
   INVALID_IMAGE_TYPE: {
@@ -24,15 +26,18 @@ const IMAGE_RESPONSE = {
   },
   RETRIEVE_SUCCESS: {
     status: "success",
+    code: "RETRIEVE_SUCCESS",
     message: "Image(s) successfully retrieved.",
   },
   RETRIEVE_ERROR: {
     status: "error",
+    code: "RETRIEVE_ERROR",
     message:
       "Could not retrieve image, please check your request and try again.",
   },
   INVALID_IMAGE_ID: {
     status: "error",
+    code: "INVALID_IMAGE_ID",
     message: "Invalid image id received.",
   },
   SAVE_ERROR: {
